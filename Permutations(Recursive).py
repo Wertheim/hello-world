d6a300a6d13158a603dc794be0e3b2e1eac98a15
Wertheim <45364221+Wertheim@users.noreply.github.com>
2018-11-26 15:06:47 -0600

    Add files via upload
    Added sample output

--- Permutations(Recursive).py
+++ Permutations(Recursive).py
@@ -18,6 +18,8 @@ def get_permutations(string):
 
 
 print get_permutations('Autry')
+###output###
+#['Autry', 'uAtry', 'utAry', 'utrAy', 'utryA', 'Atury', 'tAury', 'tuAry', 'turAy', 'turyA', 'Atruy', 'tAruy', 'trAuy', 'truAy', 'truyA', 'Atryu', 'tAryu', 'trAyu', 'tryAu', 'tryuA', 'Aurty', 'uArty', 'urAty', 'urtAy', 'urtyA', 'Aruty', 'rAuty', 'ruAty', 'rutAy', 'rutyA', 'Artuy', 'rAtuy', 'rtAuy', 'rtuAy', 'rtuyA', 'Artyu', 'rAtyu', 'rtAyu', 'rtyAu', 'rtyuA', 'Auryt', 'uAryt', 'urAyt', 'uryAt', 'urytA', 'Aruyt', 'rAuyt', 'ruAyt', 'ruyAt', 'ruytA', 'Aryut', 'rAyut', 'ryAut', 'ryuAt', 'ryutA', 'Arytu', 'rAytu', 'ryAtu', 'rytAu', 'rytuA', 'Autyr', 'uAtyr', 'utAyr', 'utyAr', 'utyrA', 'Atuyr', 'tAuyr', 'tuAyr', 'tuyAr', 'tuyrA', 'Atyur', 'tAyur', 'tyAur', 'tyuAr', 'tyurA', 'Atyru', 'tAyru', 'tyAru', 'tyrAu', 'tyruA', 'Auytr', 'uAytr', 'uyAtr', 'uytAr', 'uytrA', 'Ayutr', 'yAutr', 'yuAtr', 'yutAr', 'yutrA', 'Aytur', 'yAtur', 'ytAur', 'ytuAr', 'yturA', 'Aytru', 'yAtru', 'ytAru', 'ytrAu', 'ytruA', 'Auyrt', 'uAyrt', 'uyArt', 'uyrAt', 'uyrtA', 'Ayurt', 'yAurt', 'yuArt', 'yurAt', 'yurtA', 'Ayrut', 'yArut', 'yrAut', 'yruAt', 'yrutA', 'Ayrtu', 'yArtu', 'yrAtu', 'yrtAu', 'yrtuA']
         
         
     
